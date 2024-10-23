d78598dde0cd5f5f8b50f8265fa304764cd26c1c
Create_first_repo <gauravchoundiye123@gmail.com>
2024-10-23 15:03:57 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+ const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
+  const[data,setData]=useState(fruits)
+  const[searchData,setSearchData]=useState(data)
+  const[search,setSearch]=useState("")
+  function searchHandler(e){
+    const searchValue = e.target.value.toLowerCase();
+    setSearch(searchValue);
+    const result = data.filter(item => item.toLowerCase().includes(searchValue));
+    setSearchData(result);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input type="text" onChange={searchHandler}/>
+        {
+          searchData.map((item,id)=>{
+            return  <div className="" key={id}> 
+              <ul>
+                <li>{item}</li>
+              </ul>
+            </div>
+          })
+        }
     </div>
   )
 }
